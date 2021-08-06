9a8fe325a9bc6fc3e70951ae12a90b6a80b36486
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-08-06 16:49:05 -0400

    trying out more frontend stuff

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ console.log("test")
 //knows what request is always being sent too
 const baseUrl = "http://127.0.0.1:3000"
 
+
 const formContainer = document.querySelector(".game-container");
 //call formContainer becuase it gamecontainer has be queried
 const createGameButton = formContainer.querySelector(".create-game")
@@ -15,8 +16,10 @@ const createGameButton = formContainer.querySelector(".create-game")
 
 const currentGames = document.querySelector(".current-games");
 
+const pipelineGames = document.querySelector(".pipeline-games");
+
 //this is a comment block
-/**=======================create game file================================= */
+/**=======================create game file=================================*/
 
 const createForm = () => {
     const gameForm = document.createElement("form");
@@ -65,13 +68,54 @@ const saveGame = (event) => {
 /**============================================================ */
 
 const getAllGame = () => {
-    fetch(`${baseUrl}/creators`)
-    .then((resp) => {
-        resp.json();
-    })
+    fetch(`${baseUrl}/games`)
+    .then((resp) => resp.json())  
     .then((games) => {
-        console.log(games);
+        if (games.length > 0) {
+        renderGames(games);
+        } else {
+            console.log('no games')
+        }
+    }).catch((err) => {
+        console.log(err)
     });
 };
 
+
+
 getAllGame();
+
+/*<h4>Creator</h4>
+<p>Publish_Date</p>
+<p>Publisher</p>
+<p>Player_Count</p>
+<span>Title by Creator</span>*/
+
+const renderGames = (games) => {
+    games.forEach((game, idx) => {
+        const {creator_id, publish_date, publisher, player_count, title, creator} = game
+        const template = `
+        <h4 class = '${idx}'> ${creator_id}</h4>
+        <span>${title} by ${creator}</span>
+        <p>${publish_date}</p>
+        <p>${publisher}</p>
+        <p>${player_count}</p>
+        `;
+        pipelineGames.innerHTML += template;
+    });   
+};
+
+const postGame = () => {
+    fetch(`${baseUrl}/games`)
+    .then((resp) => resp.json())  
+    .then((games) => {
+        if (games.length > 0) {
+        renderGames(games);
+        } else {
+            console.log('no games')
+        }
+    }).catch((err) => {
+        console.log(err)
+    });
+
+}
